e7ba4f3b7ce14e7d779399ed600d6fb840fb308c
Serge <serge.mazille@gmail.com>
2016-04-15 18:32:42 +0200

    modified: welcome page (temporary)

---
+++ resources/views/home/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Robotator</title>
+    </head>
+    <body>
+    <p>Site en cours de développement</p>
+    <p><a href="http://www.robotator.fr/documentation/documentation.html">Voir la documentation (en cours d'écriture)</a></p>
+    <script></script>
+    </body>
+</html>
